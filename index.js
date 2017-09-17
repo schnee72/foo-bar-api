e23a1e16a2122981b9a9d41ae4cc818e0505e01b
Dustin Schnee <schnee72@gmail.com>
2017-09-16 23:20:37 -0500

    express

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const morgan = require('morgan');
 const compression = require('compression');
 const path = require('path');
 
+// TODO - setup router...
+
 const app = express();
 const port = process.env.PORT || 1337;
 
 app.use(helmet());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(compression());
+
+if (process.env.NODE_ENV === 'production')
+	app.use(morgan('combined'));
+else
+	app.use(morgan('dev'));
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+app.listen(port, err => {
+	if (err)
+		console.log(err);
+	else
+		console.log(`Listening on port ${port}`);
+});
