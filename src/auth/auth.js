ccf047777726a89ca60e8be7ee1a0724ca69bdc8
Dustin Schnee <schnee72@gmail.com>
2017-09-17 19:39:40 -0500

    auth

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,9 +1,19 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const register = (req, done) => {
-	bcrypt.hash(req.body.password, saltRounds)
+const register = (credentials, done) => {
+	// TODO - make sure user doesn't already exist
+	bcrypt.hash(credentials.password, saltRounds)
 		.then(hash => done(hash));
+	// Insert user in db
 }
+
+// const login = (credentials, done) => {
+	// TODO - check user in db and then generate a jwt and return it
+// };
+
+// const verify = (jwt, done) => {
+	// TODO - verify that the jwt is legit
+// };
 
 module.exports = {register};
--- src/auth/authRouter.js
+++ src/auth/authRouter.js
@@ -5,15 +5,15 @@ const auth = require('./auth');
 router.get('/', (req, res) => res.send('auth!'));
 
 router.post('/register', (req, res) => {
-	auth.register(req, h => res.json({h: h}));
+	auth.register(req.body.credentials, hash => res.json({hash: hash}));
 });
 
 router.post('/login', (req, res) => {
-
+	res.send('login!');
 });
 
 router.post('/verify', (req, res) => {
-
+	res.send('verify!');
 });
 
 module.exports = router;
