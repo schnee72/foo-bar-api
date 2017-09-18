92d993515208cf684778441bd4080528f32dab4d
Dustin Schnee <schnee72@gmail.com>
2017-09-17 20:25:33 -0500

    auth

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -4,9 +4,10 @@ const saltRounds = 10;
 const register = (credentials, done) => {
 	// TODO - make sure user doesn't already exist
 	bcrypt.hash(credentials.password, saltRounds)
-		.then(hash => done(hash));
+		.then(hash => done({status: 201, msg: {hash: hash}}))
+		.catch(err => done({status: 500, msg: {error: err}}));
 	// Insert user in db
-}
+};
 
 // const login = (credentials, done) => {
 	// TODO - check user in db and then generate a jwt and return it
--- src/auth/authRouter.js
+++ src/auth/authRouter.js
@@ -1,12 +1,19 @@
+/* eslint-disable no-console */
 const express = require('express');
 const router = express.Router();
 const auth = require('./auth');
 
+router.use((req, res, next) => {
+	if (req.headers.authorization !== process.env.AUTH_KEY)
+		res.status(401).json({"error": "unauthorized"});
+	next();
+});
+
 router.get('/', (req, res) => res.send('auth!'));
 
-router.post('/register', (req, res) => {
-	auth.register(req.body.credentials, hash => res.json({hash: hash}));
-});
+router.post('/register', (req, res) => 
+	auth.register(req.body.credentials, ret => 
+		res.status(ret.status).json(ret.msg)));
 
 router.post('/login', (req, res) => {
 	res.send('login!');
