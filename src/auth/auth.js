437c19bef658b04d554d094660e29600e38d093b
Dustin Schnee <schnee72@gmail.com>
2017-09-18 22:36:53 -0500

    auth

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -5,30 +5,30 @@ const sql = require('../sqlServer');
 const saltRounds = 10;
 
 const register = (credentials, done) => {
-  if (!(credentials.name || credentials.password))
-    return done({ status: 412, msg: { error: 'id and password are required' } });
+  if (missingCredentials(credentials))
+    return done(errorMessage(412, 'name and password are required'));
   sql.getUser(credentials.name, result => {
     if (result.recordset.length > 0) // exits
-      return done({ status: 409, msg: { errror: 'conflict' } });
+      return done(errorMessage(409, 'conflict'));
     else // doesn't exist
       bcrypt.hash(credentials.password, saltRounds)
         .then(hash => {
           sql.addUser(credentials.name, hash, () => {
             jwt.sign(getPayload(credentials.name), process.env.JWT_KEY, (err, token) => {
               if (err)
-                done({status: 500, msg: { error: err }});
+                done(errorMessage(500, err));
               else
                 done({ status: 201, msg: { jwt: token } });
             });
           });
         })
-        .catch(err => done({ status: 500, msg: { error: err } }));
+        .catch(err => done(errorMessage(500, err)));
   });
 };
 
 const login = (credentials, done) => {
-  if (!(credentials.name || credentials.password))
-    return done({ status: 412, msg: { error: 'id and password are required' } });
+  if (missingCredentials(credentials))
+    return done(errorMessage(412, 'name and password are required'));
   sql.getUser(credentials.name, result => {
     if (result.recordset.length > 0) // exits
       bcrypt.compare(credentials.password, result.recordset[0].hash)
@@ -36,15 +36,16 @@ const login = (credentials, done) => {
           if (res)
             jwt.sign(getPayload(credentials.name), process.env.JWT_KEY, (err, token) => {
               if (err)
-                done({status: 500, msg: { error: err }});
+                done(errorMessage(500, err));
               else
                 done({ status: 201, msg: { jwt: token } });
             });
           else
-            done({ status: 401, msg: { error: 'unauthorized' } });
-        });
+            done(errorMessage(401, 'unauthorized'));
+        })
+        .catch(err => done(errorMessage(500, err)));
     else // doesn't exist
-      return done({ status: 404, msg: { errror: 'not found' } });
+      return done(errorMessage(404, 'not found'));
   });
 };
 
@@ -58,5 +59,13 @@ const getPayload = (val) => {
     data: val
   };
 };
+
+const missingCredentials = credentials => {
+  return !credentials.name || !credentials.password;
+};
+
+const errorMessage = (status, err) => {
+  return {status: status, msg: {error: err}};
+}
 
 module.exports = { register, login };
